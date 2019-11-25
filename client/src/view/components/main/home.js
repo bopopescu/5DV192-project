f26cc6572b0c8cc59015d30acc693d4480f6e0ec
Kaj Nygren <c15knn@cs.umu.se>
2019-11-25 12:26:33 +0100

    update

--- client/src/view/components/main/home.js
+++ client/src/view/components/main/home.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
 import { Wrapper } from "../../containers/wrapper";
+import $ from 'jquery';
 
 
 class Home extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+
+    }
+
+    handleClick() {
+
+        let filename = $('input[type=file]').val().split('\\').pop();
+        $('#file-name').html(filename);
+
+    }
 
     render() {
         return (
             <Wrapper>
                 <div className="content-inner">
-                    <input type="file" name="file" id="file" onChange=""/>
+                    <div className="file-input">
+                        <input type="file" id="file-upload" onChange={() => this.handleClick()} />
+                        <span className="label" id="file-name">No file selected</span>
+                        <span className="button">Choose</span>
+                    </div>
                 </div>
             </Wrapper>
         )
