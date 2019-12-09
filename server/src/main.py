1e817a8f265c52ecc416f876a665f13e06a540af
Kaj Nygren <c15knn@cs.umu.se>
2019-12-09 15:17:19 +0100

    update

--- server/src/main.py
+++ server/src/main.py
@@ -14,55 +14,45 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 @app.route('/transcode/request', methods=['POST'])
 def form_example():
 
+    print("----------------------------------")
+    print("TRANSCODE REQUEST")
+    print("----------------------------------")
+
     if request.method == 'POST':
 
-        # if request is ok, send back 200 response with token
-
-        print("Received: 200")
-        data = request.get_json(silent=True)
-
-        print("----------------------------------")
-        print("TRANSCODE REQUEST")
-        print("----------------------------------")
-        print(">>> RECEIVED FILE:")
-        print(request.files['file'])
-        print(">>> RECEIVED FORM DATA:")
-
-        formData = request.form.to_dict(flat=True)
-        print(formData)
-
-        target = os.path.join(app.root_path, UPLOAD_FOLDER)
-        if not os.path.isdir(target):
-            os.mkdir(target)
+        print(">>> RECEIVED:")
 
         file = request.files['file']
+        form = request.form
 
-        destination = "/".join([target, formData.get("input-filename")])
-        file.save(destination)
+        print(file)
+        print(form)
 
-        data = {
-            "token": "123",
-        }
+        save_file(file, form)
 
-        print(">>> SENT 200")
+        response = {"status": "success"}
+        response_code = 200
 
-        response = app.response_class(
-            response=json.dumps(data),
-            status=200,
-            mimetype='application/json'
-        )
-        return response
+    else:
 
-    print(">>> SENT 201")
+        response = {"error": "invalid method"}
+        response_code = 201
 
-    print("other method received")
-    response = app.response_class(
-        response=json.dumps({"error": "invalid method"}),
-        status=201,
-        mimetype='application/json'
-    )
+    print(">>> SENT:")
+    response = app.response_class(response=json.dumps(response), status=response_code, mimetype='application/json')
     return response
 
+
+def save_file(file, form):
+
+    form_data = form.to_dict(flat=True)
+
+    target = os.path.join(app.root_path, UPLOAD_FOLDER)
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    destination = "/".join([target, form_data.get("input-filename")])
+    file.save(destination)
 
 
 @app.route('/')
