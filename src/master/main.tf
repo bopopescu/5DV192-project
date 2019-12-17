70f56ba75cddfc315e98e987fe43b3789b737770
Kaj Nygren <c15knn@cs.umu.se>
2019-12-17 18:25:56 +0100

    update

--- src/master/main.tf
+++ src/master/main.tf
@@ -2,4 +2,25 @@ provider "google" {
   credentials = "${file("credentials.json")}"
   project     = "testproject-261510"
   region      = "europe-north1"
+  zone        = "europe-north1-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+
+  name         = "master-1"
+  machine_type = "n1-standard-1"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config {
+    }
+  }
+
 }
