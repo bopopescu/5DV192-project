55b58eab6365a4cb5c3826f54ae25a6638b51047
Kaj Nygren <c15knn@cs.umu.se>
2019-12-17 18:16:04 +0100

    update

--- src/master/build.sh
+++ src/master/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=$PATH:../../apps/google-cloud-sdk/bin
 
-gcloud auth activate-service-account docker@testproject-261510.iam.gserviceaccount.com --key-file=credentials-docker.json --project=testproject
+gcloud auth activate-service-account superadmin@testproject-261510.iam.gserviceaccount.com --key-file=credentials.json --project=testproject
 
 gcloud config set project 'testproject-261510'
 
