6d2f5f0f48c35f0c92110116d369652cad08cc32
Erik Dahlberg <c15edg@cs.umu.se>
2020-01-07 19:07:24 +0100

    added terafore for split node

--- src/clients/client/src/controller/api.js
+++ src/clients/client/src/controller/api.js
@@ -2,7 +2,7 @@ import apisauce from "apisauce";
 import Cookie from "universal-cookie";
 
 const API_URL= "http://" + window.location.hostname + ":5000";
-const API_EXTERNAL_URL= "https://35.223.77.15/" + ":5000";
+const API_EXTERNAL_URL= "http://" + "35.228.95.170" + ":5000";
 
 let create = () => {
 
---
+++ src/workers/split/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo curl -sSL https://get.docker.com/ | sh
+sudo apt-get --yes --force-yes install ffmpeg
+sudo usermod -aG docker `echo $USER`
+sudo docker run -d -p 5000:5000 gcr.io/testproject-261510/splitimage1:latest
+
