d98836fb2d14cad49064bfc3528f301a0b848fb2
Kaj Nygren <c15knn@cs.umu.se>
2020-01-08 14:23:42 +0100

    update

---
+++ src/workers/split/docker-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# config
+
+GCLOUD_USERNAME="superadmin"
+GCLOUD_PROJECT="testproject-261510"
+GCLOUD_BUILD_NAME="split"
+
+GCLOUD_FILE_CREDENTIALS="credentials.json"
+
+# env
+
+PATH=$PATH:../../../apps/google-cloud-sdk/bin
+
+# runtime
+
+gcloud auth activate-service-account ${GCLOUD_USERNAME}@${GCLOUD_PROJECT}.iam.gserviceaccount.com --key-file=${GCLOUD_FILE_CREDENTIALS} --project=${GCLOUD_PROJECT}
+gcloud config set project ${GCLOUD_PROJECT}
+gcloud builds submit --tag gcr.io/${GCLOUD_PROJECT}/${GCLOUD_BUILD_NAME}
