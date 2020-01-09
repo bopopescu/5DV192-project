b7b318283cab48ff944ebc2ce8c497a32d4579e6
c15aem <c15aem@cs.umu.se>
2020-01-09 19:12:49 +0100

    upload and convert working together

--- src/workers/split/src/download_dir/splitter.sh
+++ src/workers/split/src/download_dir/splitter.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mkdir $2
+mkdir $2/$3
 
-ffmpeg -i $1 -vcodec copy -map 0  -reset_timestamps 1 -sc_threshold 0 -force_key_frames "expr:gte(t,n_forced*1)" -f segment -segment_time 10 $2/$2_%03d.mp4
+ffmpeg -i $1 -vcodec copy -map 0  -reset_timestamps 1 -sc_threshold 0 -force_key_frames "expr:gte(t,n_forced*1)" -f segment -segment_time 10 $2/$3/$3_%03d.mp4
 
-cd $2
+cd $2/$3
 for file in $PWD/*; do
   echo file "'./"$(basename $file)"'" >> $3.txt
 done
-echo $2
+echo $3
