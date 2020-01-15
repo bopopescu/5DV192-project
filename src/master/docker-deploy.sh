cbaf28904befa326e08af235b2b0ebc2deabc36f
c15aem <c15aem@cs.umu.se>
2020-01-15 19:17:29 +0100

    update prometheus

--- src/master/docker-deploy.sh
+++ src/master/docker-deploy.sh
@@ -6,7 +6,7 @@ GCLOUD_USERNAME="superadmin"
 GCLOUD_PROJECT="testproject-261510"
 GCLOUD_BUILD_NAME="master"
 
-GCLOUD_FILE_CREDENTIALS="credentials.json"
+GCLOUD_FILE_CREDENTIALS="../../config/credentials.json"
 
 # env
 
--- src/prometheus/init.sh
+++ src/prometheus/init.sh
@@ -3,4 +3,5 @@ sudo apt-get update
 sudo curl -sSL https://get.docker.com/ | sh
 sudo systemctl start docker
 sudo docker run -p 9090:9090 gcr.io/testproject-261510/prometheus:latest
+sudo docker run -d -p 3000:3000 grafana/grafana
 
